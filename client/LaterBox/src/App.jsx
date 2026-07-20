af821f5749ecffb7cf9a2154d36d8184d93d60fb
Andrei Gobres <andreigobres3@gmail.com>
2026-07-20 22:42:04 +0800

    Wrap the routes in UserContext and BookmarkContext

--- client/LaterBox/src/App.jsx
+++ client/LaterBox/src/App.jsx
@@ -8,23 +8,26 @@ import BookmarkDetail from "./pages/saved-links/BookmarkDetail";
 import Settings from "./pages/settings/Settings";
 import AppShell from "./pages/layout/AppShell";
 import UserProvider from "./contexts/UserContext";
+import BookmarkProvider from "./contexts/BookmarkContext";
 
 function App() {
   return (
     <UserProvider>
-      <BrowserRouter basename="/LaterBox">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/signup" element={<CreateAccount />} />
-          <Route path="/login" element={<SignIn />} />
-          <Route element={<AppShell />}>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/saved-links" element={<SavedLinks />} />
-            <Route path="/saved-links/:id" element={<BookmarkDetail />} />
-            <Route path="/settings" element={<Settings />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <BookmarkProvider>
+        <BrowserRouter basename="/LaterBox">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/signup" element={<CreateAccount />} />
+            <Route path="/login" element={<SignIn />} />
+            <Route element={<AppShell />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/saved-links" element={<SavedLinks />} />
+              <Route path="/saved-links/:id" element={<BookmarkDetail />} />
+              <Route path="/settings" element={<Settings />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </BookmarkProvider>
     </UserProvider>
   );
 }
