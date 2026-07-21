3cc6fdd70b15bb6c16c4f83f9b3d9477e9b17a09
Andrei Gobres <andreigobres3@gmail.com>
2026-07-21 21:09:11 +0800

    Add a new function that handle the addition of new bookmarks

--- server/controllers/bookmarkController.js
+++ server/controllers/bookmarkController.js
@@ -17,7 +17,7 @@ const getBookmarks = async (req, res) => {
                     is_visited: bookmark.is_visited === 1,
                     is_starred: bookmark.is_starred === 1,
                     is_private: bookmark.is_private === 1,
-                    tags: bookmark.tags.split(',')
+                    tags: bookmark.tags?.split(',')
                 }
             })
         );
@@ -27,6 +27,43 @@ const getBookmarks = async (req, res) => {
     }
 }
 
+const addBookmark = async (req, res) => {
+    try {
+        const { title, url, platform, note, tags } = req.body;
+
+        const [result] = await db.promise().query(
+            `INSERT INTO bookmarks (user_id, title, url, platform, note) VALUES (?, ?, ?, ?, ?)`,
+            [req.session.userId, title, url, platform, note]
+        );
+
+        const bookmark_id = result.insertId;
+
+        for(const tag of tags) {
+            const [result] = await db.promise().query(
+                `INSERT INTO tags (tag)
+                 VALUES (?)
+                 ON DUPLICATE KEY UPDATE
+                 tag_id = LAST_INSERT_ID(tag_id)`,
+                 [tag]
+            );
+
+            const tag_id = result.insertId;
+
+            await db.promise().query(
+                `INSERT INTO bookmark_tags
+                 VALUES (?, ?)`,
+                 [bookmark_id, tag_id]
+            );
+        }
+
+        return res.status(201).json({ message: "Bookmark added", bookmark_id: result.insertId });
+    } catch(err) {
+        console.error(err);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
 export const bookmarkControllers = {
-    getBookmarks
+    getBookmarks,
+    addBookmark
 }
