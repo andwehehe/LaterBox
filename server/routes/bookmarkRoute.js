14f9238dc49765a9d52a66fbe43572a25aff9333
Andrei Gobres <andreigobres3@gmail.com>
2026-07-24 21:20:54 +0800

    Add 2 new route for 2 new request function from bookmarkService and uses 2 new controllers from bookmarkController

--- server/routes/bookmarkRoute.js
+++ server/routes/bookmarkRoute.js
@@ -3,9 +3,11 @@ import { requireAuth } from "../middlewares/requireAuth.js";
 import { bookmarkControllers } from "../controllers/bookmarkController.js";
 
 const bookmarkRouter = express.Router();
-const { getBookmarks, addBookmark } = bookmarkControllers;
+const { getBookmarks, addBookmark, getTargetBookmark, updateTags } = bookmarkControllers;
 
 bookmarkRouter.get('/', requireAuth, getBookmarks);
 bookmarkRouter.post('/addbookmark', requireAuth, addBookmark);
+bookmarkRouter.get('/:bookmark_id', requireAuth, getTargetBookmark);
+bookmarkRouter.patch('/:bookmark_id', requireAuth, updateTags);
 
 export default bookmarkRouter;
