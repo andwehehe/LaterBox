ff0e19dc24e9fda5c07531acb6be8081cea7f748
Andrei Gobres <andreigobres3@gmail.com>
2026-08-07 20:09:31 +0800

    Rename authRoute to auth.route

---
+++ server/routes/auth.route.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { authControllers } from '../controllers/auth.controller.js';
+import { requireAuth } from '../middlewares/requireAuth.js';
+
+const authRouter = express.Router();
+const { registerAccount, loginAccount, getUserData } = authControllers;
+
+authRouter.post('/register', registerAccount);
+authRouter.post('/login', loginAccount);
+authRouter.get('/me', requireAuth, getUserData);
+
+export default authRouter;
