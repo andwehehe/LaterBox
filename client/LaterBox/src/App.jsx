2d5dc61b8a618b3510ccbdc2bb972bf17418b6bd
Andrei Gobres <andreigobres3@gmail.com>
2026-09-05 19:18:47 +0800

    update: deleted settings page

--- client/LaterBox/src/App.jsx
+++ client/LaterBox/src/App.jsx
@@ -5,7 +5,6 @@ import CreateAccount from "./pages/account/CreateAccount";
 import SignIn from "./pages/account/SignIn";
 import SavedLinks from "./pages/saved-links/SavedLinks";
 import BookmarkDetail from "./pages/saved-links/BookmarkDetail";
-import Settings from "./pages/settings/Settings";
 import AppShell from "./pages/layout/AppShell";
 import UserProvider from "./contexts/UserContext";
 import BookmarkProvider from "./contexts/BookmarkContext";
@@ -24,7 +23,6 @@ function App() {
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/saved-links" element={<SavedLinks />} />
               <Route path="/saved-links/:bookmark_id/:title" element={<BookmarkDetail />} />
-              <Route path="/settings" element={<Settings />} />
               <Route path="/profile" element={<Profile />} />
             </Route>
           </Routes>
